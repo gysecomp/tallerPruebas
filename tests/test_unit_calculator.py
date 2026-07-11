92c0e627ce4bcc366003eee4a3e772ba78d6b6e1
gysecomp <gysecomp@gmail.com>
2026-07-11 08:39:51 -0500

    primeras pruebas unitarias

---
+++ tests/test_unit_calculator.py
@@ -0,0 +1,11 @@
+from src.calculator import suma, resta
+
+def test_suma():
+    assert suma(2, 3) == 5
+    assert suma(-1, 1) == 0
+    assert suma(0, 0) == 0
+
+def test_resta():
+    assert resta(5, 3) == 2
+    assert resta(0, 0) == 0
+    assert resta(-1, -1) == 0
