90b241aa96ba15c0af96fa36ba130a5ed2112077
vlacaicedo77 <vlacaicedo77@gmail.com>
2026-07-11 08:58:22 -0500

    multiplicar

--- src/calculator.py
+++ src/calculator.py
@@ -4,12 +4,10 @@ def suma(a, b):
 def resta(a,b):
     return a-b
 
-<<<<<<< HEAD
 def division(a, b):
     if b == 0:
         return "Error: División por cerro no está definido"
     return a / b
-=======
+
 def multiplicar(a,b):
-    return a-b   
->>>>>>> 9e79c6e0f034f73b89382ed08e9da9ac1cfba96a
+    return a-b
